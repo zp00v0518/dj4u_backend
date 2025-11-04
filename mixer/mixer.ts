1c45f81a26388bc464f634ad2c738db4b252309e
Dmytro <johndoe@example.com>
2025-11-04 19:36:43 +0200

    fix command

--- mixer/mixer.ts
+++ mixer/mixer.ts
@@ -3,7 +3,6 @@ import { promisify } from "node:util";
 
 import path from "node:path";
 import config from "../config/config.ts";
-import os from "node:os";
 
 const execPromise = promisify(exec);
 
@@ -15,21 +14,22 @@ class Mixer {
 
     const mixName = `mix_${Date.now()}`;
     const mixPath = path.join(config.files.mixesDir, userID, mixName);
-    const fullCommand =
-      os.platform() === "win32"
-        ? `cd mixer\\app & mix1.exe ${mixPath} ${filesPath.join(" ")}`
-        : `cd  /home && ./mix1 ${mixPath} ${filesPath.join(" ")}`;
-    // for Linux
-    // const fullCommand = `cd  /home && ./mix1 ${mixPath} ${filesPath.join(" ")}`;
-    
+      //   const fullCommand =
+      // os.platform() === "win32"
+      //   ? `cd mixer\\app & mix1.exe ${mixPath} ${filesPath.join(" ")}`
+      //   : `cd  /home && ./mix1 ${mixPath} ${filesPath.join(" ")}`;
+    //for Linux
+    const fullCommand = `cd  /home && ./mix1 ${mixPath} ${filesPath.join(
+      " "
+    )}`;
     // const fullCommand = `cd mixer\\app & mix1.exe ${mixPath} ${filesPath.join(
     //   " "
     // )}`;
 
     try {
       const { stdout, stderr } = await execPromise(fullCommand);
-      if (!stdout.includes("Command: python convert.py")) {
-        // if (!stdout.includes("Conversion successful!")) {
+    if (!stdout.includes("Command: python convert.py")) {
+      // if (!stdout.includes("Conversion successful!")) {
         // console.log('stdout >>>>    ', stdout)
         // console.log('stderr >>>>    ', stderr)
         return false;
